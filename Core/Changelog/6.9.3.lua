e60351df71bec56fb6b7459b1cfa43285fa4664a
whale <17140820@qq.com>
2025-03-30 23:49:50 +0800

    👌 更新: 汉化6.9.1-3版本变更日志

--- Core/Changelog/6.9.3.lua
+++ Core/Changelog/6.9.3.lua
@@ -3,7 +3,7 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.9.3"] = {
   HOTFIX = true,
   CHANGES = {
-    "* New features",
-    F.String.Menu.Performance() .. " tab",
+    "* 新功能",
+    F.String.Menu.Performance() .. " 选项卡",
   },
 }
